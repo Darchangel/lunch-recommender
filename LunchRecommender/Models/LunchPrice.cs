2a52aaea48b480c4fec0324d94120dd612956495
Filipe Silva <f.silva@coolblue.nl>
2016-12-04 19:58:18 +0100

    Improve display properties for LunchPrice

--- LunchRecommender/Models/LunchPrice.cs
+++ LunchRecommender/Models/LunchPrice.cs
@@ -1,9 +1,14 @@
-﻿namespace LunchRecommender.Models
+﻿using System.ComponentModel;
+
+namespace LunchRecommender.Models
 {
     public enum LunchPrice
     {
+        [DisplayName("cheaply")]
         Low,
+        [DisplayName("at an OK price")]
         Medium,
+        [DisplayName("at a higher price")]
         High
     }
 }
